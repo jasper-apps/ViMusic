f0f3928926404d29f3057cd34ca3bdf5e92948e4
pavlohural <pavlohural@gmail.com>
2022-10-17 13:00:48 +0300

    Release 21 0.4.9

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "it.vfsfitvnm.vimusic"
         minSdk = 21
         targetSdk = 32
-        versionCode = 20
-        versionName = "0.4.8"
+        versionCode = 21
+        versionName = "0.4.9"
     }
 
     splits {
