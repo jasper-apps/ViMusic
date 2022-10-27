85b77791c47d0f7cb5bc01dd496254303323b340
yterletskyi <yterletskyi@gmail.com>
2022-10-27 18:42:54 +0300

    Release 24 0.4.12

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "it.vfsfitvnm.vimusic"
         minSdk = 21
         targetSdk = 32
-        versionCode = 23
-        versionName = "0.4.11"
+        versionCode = 24
+        versionName = "0.4.12"
     }
 
     splits {
